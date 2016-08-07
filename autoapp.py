88e6d64682c1e78a4d5847d1735414faf4768cac
Matt Stanley <stanley.t.matthew@gmail.com>
2016-08-07 16:31:32 -0400

    adds basic unit tests and testing command

--- autoapp.py
+++ autoapp.py
@@ -1,8 +1,17 @@
 """create_app.py
 
-creates an instance of the stockpot app for use by the flask cli tool
+Creates an instance of the stockpot app for use by the flask cli tool.
 """
 
 import os
 from app import create_app
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
