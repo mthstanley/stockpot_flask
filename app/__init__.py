3e72c48281ea89da53e626287902dcbe763a67fb
Matt Stanley <stanley.t.matthew@gmail.com>
2016-08-07 14:49:54 -0400

    adds app factory

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from config import config
+
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # register blueprints
+
+    
+
+    #####################
+
+    return app
