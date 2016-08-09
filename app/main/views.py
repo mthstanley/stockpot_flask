11f60a5a93935090abdb5d36e782f177e006ad07
Matt Stanley <stanley.t.matthew@gmail.com>
2016-08-09 17:25:56 -0400

    adds login and logout functionality

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,7 @@ Routes and views for the main blueprint.
 """
 from flask import render_template, session, redirect, url_for
 from . import main
+from ..models import User
 
 
 @main.route('/', methods=['GET', 'POST'])
